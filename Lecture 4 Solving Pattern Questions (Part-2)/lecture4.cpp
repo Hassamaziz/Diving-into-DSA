d718ec7854c6e58179a7cb7f50e13a56be5ddd5e
hassamaziz <hassamaziz001@gmail.com>
2023-12-24 20:13:15 +0500

    lecture 4 Pattern 9 done

--- Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
+++ Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
@@ -132,6 +132,27 @@ int main(){
 
 //     }
 
+//                                       // Pattern 9               
+//         int n ; 
+//     cin >> n ;
+//     int row = 1;
+   
+
+//     while(row<=n){
+// int col = 1;
+// int value = row;
+// while (col<=row)
+// {
+//    cout << value << " ";
+//   value++;
+//    col++;
+
+// }
+// cout << endl;
+// row++;
+
+//     }
+
 
 
 
