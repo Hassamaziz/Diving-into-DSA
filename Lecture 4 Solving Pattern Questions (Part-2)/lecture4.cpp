02b3bb71b5b85aac93dec78ec871e571196ba447
hassamaziz <hassamaziz001@gmail.com>
2023-12-24 21:43:48 +0500

    lecture 4 Pattern 21 done

--- Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
+++ Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
@@ -427,4 +427,30 @@ int main(){
 //   cout << endl;
 //   row++ ;
 //     } 
+
+//                                       // Pattern 21       
+
+//         int n ; 
+//     cin >> n ;
+//     int row = 1;
+     
+//     while(row<=n){
+
+//   int col=1;
+//     int space = row-1;
+//   while (space)
+//   {
+//     cout << " ";
+//     space--;
+//   }
+//   while (col<=n-row+1)
+//   {
+//    cout << row ;
+   
+//    col++;
+//   }
+//   cout << endl;
+//   row++ ;
+//     } 
+
 }
