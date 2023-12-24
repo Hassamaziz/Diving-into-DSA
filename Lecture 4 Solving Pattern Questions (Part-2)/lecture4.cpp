a7730b960d34332d04ea5ee58106bc70dd121dfe
hassamaziz <hassamaziz001@gmail.com>
2023-12-24 21:14:20 +0500

    lecture 4 Pattern 17 done

--- Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
+++ Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
@@ -333,6 +333,29 @@ int main(){
 
 //     } 
 
+//                                       // Pattern 17        
+
+//         int n ; 
+//     cin >> n ;
+//     int row = 1;
+    
+   
+
+//     while(row<=n){
+// int col = 1;
+// char ch = 'A' + n - row;
+
+// while (col<= row ){
+//    cout << ch << " " ;
+//   ch = ch+1;
+//    col++;
+
+// }
+// cout << endl;
+// row++;
+
+//     } 
+
 
 
 }
