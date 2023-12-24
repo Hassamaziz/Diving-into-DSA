c47b4e833147d22bc1460ab92fb33b1fd95e5778
hassamaziz <hassamaziz001@gmail.com>
2023-12-24 19:15:27 +0500

    lecture 4 Pattern 4 done

--- Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
+++ Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
@@ -19,6 +19,43 @@ int main(){
 // i++;
 
 //     }
+//                                             // Pattern 4               
+//         int n ; 
+//     cin >> n ;
+//     int i = 1;
+
+//     while(i<=n){
+// int j= 1;
+// while (j<=n)
+// {
+//    cout << n-j+1;
+//    j++;
+
+// }
+// cout << endl;
+// i++;
+
+//     }
+
+//                                       // An Alternative Method For Pattern 4 
+//         int n ; 
+//     cin >> n ;
+//     int i = 1;
+
+//     while(i<=n){
+// int j= n;
+// while (j<=n && j!=0)
+// {
+//    cout << j;
+//    j--;
+
+// }
+// cout << endl;
+// i++;
+
+//     }
+
+
 
                                                
 
