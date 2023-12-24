9067856fe0701fbdac423cf46d41891bde8b3a04
hassamaziz <hassamaziz001@gmail.com>
2023-12-24 15:03:09 +0500

    first commit

---
+++ Lecture 2 Hello World/helloworld.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+int main() {
+                                        // BASIC HELLO WORLD PROGRAM
+    // cout << "Salaam Duniya" << endl;
+    // cout << "Salaam Duniya" << '\n';
+    // cout << "Using other ways to endline\n";
+
+
+//                                              // DATA TYPES
+
+//                                            // Integer Type
+// int a = 420;
+// cout << a << endl;
+ 
+//                                         //  Character Type
+// char b = 's';
+// cout << b <<endl;
+
+//                                         // Boolean Type
+// bool bl = true;
+// cout<< bl <<endl;
+
+// bool fl = false;
+// cout<< fl <<endl;
+
+//                                         // Float Type
+
+// float f = 1.25;
+// cout << f << endl;
+
+                                        // Double Type
+// double d = 1.245;
+// cout << d <<endl; 
+    
+                                        // Size of
+    // int sizea = sizeof(a);
+    // cout << "size of a is " << sizea <<endl;  
+    // int sizeb = sizeof(b);
+    // cout << "size of b is " << sizeb <<endl; 
+    // int sizebl = sizeof(bl);
+    // cout << "size of bl is " << sizebl <<endl; 
+    // int sizef = sizeof(f);
+    // cout << "size of f is " << sizef <<endl; 
+    // int sized = sizeof(d);
+    // cout << "size of d is " << sized <<endl; 
+
+
+  
+
+                                                // Operators
+    // int a = 2 / 5;
+    // cout << a << endl;
+
+    //     double b = 2.0 / 5;
+    //     cout << b << endl;
+    
+    //     cout << 2.0 / 5 << endl;
+
+                                            // Relational Operators
+
+    //     int a = 2;
+    //     int b = 2;
+    
+    //     bool first = a == b;
+    //     cout << first << endl;
+
+    // bool second = a > b;
+    // cout << second << endl;
+    // bool third = a >= b;
+    // cout << third << endl;
+
+    // int h = 0;
+    // cout << !h << endl;
+
+
+}
---
+++ Lecture 3 If-Else, While loop & Lots of Patterns (Part-1)/lecture3.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    // int n;
+//  cout << "Enter the number of elements: ";
+//     cin>>n;
+//     cout << "value of n is "<< n <<endl;
+
+
+// int a;
+// cin>>a;
+
+// if(a>0 ) {
+//     cout << "A is Positive!!!!!" << endl;
+// }
+// else {
+//     cout << "A is negative" << endl;
+// }
+
+//                                        // if condition 
+
+// int a , b;
+
+// cout << "enter the value of a  "<< endl;
+// cin >> a;
+// cout << "enter the value of b " << endl;
+// cin >> b;
+ 
+//  if (a > b)
+//  {
+//     cout << "Value of a is greater than Value of b";
+//  }
+//  else if (b > a)
+//  {
+//     cout << "Value of b is greater than Value of a";
+//  }
+ 
+//                                                 // Is Number Positive ,negative or null by if else  
+
+
+// int a;
+// cin>>a;
+
+// if(a>0 ) {
+//     cout << "A is Positive!!!!!" << endl;
+// }
+
+// else{
+
+// if(a<0) {
+//     cout << "A is negative" << endl;
+// }
+// else
+// {
+//      cout << "A is zero" << endl;
+// }
+  
+// }
+
+//                                                 // Is Number Positive ,negative or null by else if  
+
+
+// int a;
+// cin>>a;
+
+// if(a>0 ) {
+//     cout << "A is Positive!!!!!" << endl;
+// }
+// else if (a<0){
+//     cout << "A is Negative" << endl;
+// }
+// else{
+//     cout << "A is zero "<< endl;
+// }
+
+
+//                                                  // Home word question 1
+
+//  int a = 24;
+
+//  if (a < 20)
+//  {
+//     cout << "Love"<< endl;
+//  }
+//  else if (a == 24)
+//  {
+//     cout << "Lovely" << endl;
+//  }
+//  else
+//  {
+//    cout << "Babbar" << endl;
+//  }
+
+ //                                                //Home work Question 2
+
+ char c;
+ cout << "enter a character"<< endl;
+ cin >> c;
+
+  if (int(c) >= 97 && int(c) <= 122)
+ {
+    cout << "This is LowerCase"<< endl;
+ }
+ else if (int(c) >= 65 && int(c) <= 90)
+ {
+    cout << "This is Uppercase" << endl;
+ }
+ else if(int(c) >= 48 && int(c) <= 57)
+ {
+   cout << "This is numeric" << endl;
+ }
+
+
+
+
+}
