cebe817651437a70bfc380bb4b0f80da4c45bec9
hassamaziz <hassamaziz001@gmail.com>
2023-12-24 19:58:37 +0500

    lecture 4 Pattern 8 done

--- Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
+++ Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
@@ -112,6 +112,27 @@ int main(){
 //     }
 
 
+//                                       // Pattern 8               
+//         int n ; 
+//     cin >> n ;
+//     int i = 1;
+//     int count = 1;
+
+//     while(i<=n){
+// int j= 1;
+// while (j<=i)
+// {
+//    cout << count << " ";
+//    count++;
+//    j++;
+
+// }
+// cout << endl;
+// i++;
+
+//     }
+
+
 
 
 
