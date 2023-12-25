4f38105050a8dbcca671a0342c39ee7120a5c796
hassamaziz <hassamaziz001@gmail.com>
2023-12-25 18:51:32 +0500

    Lecture 5 starting

---
+++ Lecture 5 Bitwise Operators, For Loops, Operator Precedence & Variable Scoping/lecture5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+                                   // AND OR NOT XOR 
+    // int a =4;
+    // int b = 6;
+
+    //  cout<<" a&b "<< (a&b) << endl;
+    //  cout<<" a|b "<< (a|b) << endl;
+    //  cout<<" ~a "<< (~a) << endl;
+    //  cout<<" a^b "<< (a^b) << endl;
+}
