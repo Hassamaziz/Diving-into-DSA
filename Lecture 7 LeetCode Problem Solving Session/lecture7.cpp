d233c0490c978586cead163cffafcbdcc0a9a62f
hassamaziz <hassamaziz001@gmail.com>
2023-12-26 14:57:26 +0500

    2nd question done

--- Lecture 7 LeetCode Problem Solving Session/lecture7.cpp
+++ Lecture 7 LeetCode Problem Solving Session/lecture7.cpp
@@ -2,27 +2,48 @@
 using namespace std;
 
 int main(){
-int x;
-cout << "Enter a number: ";
-cin >> x;
+
+                                            // 1st question 
+// int x;
+// cout << "Enter a number: ";
+// cin >> x;
 
 
-int ans = 0;
-while (x!=0)
-{
-    int digit = x % 10;
+// int ans = 0;
+// while (x!=0)
+// {
+//     int digit = x % 10;
    
-    ans = ans * 10 +digit;
-    x /= 10;
-    if ((ans>INT_MAX/10)||(ans<INT_MIN/10))
-    {
-        return 0;
-    }
+//     ans = ans * 10 +digit;
+//     x /= 10;
+//     if ((ans>INT_MAX/10)||(ans<INT_MIN/10))
+//     {
+//         return 0;
+//     }
  
 
     
-}
-cout << ans;
+// }
+// cout << ans;
+
+
+        //                                          // 2nd question
+        // int n;
+        // cin >> n;
+        // int m = n;
+        // int mask = 0;
+        // if (n == 0)
+        // {
+        //     return 1;
+        // }
+        // while (m != 0)
+        // {
+        //     mask = (mask << 1) | 1;
+        //     m = m >> 1;
+        // }
+        // int ans = (~n) & mask;
+
+        // cout<<  ans;
 
 
 }
