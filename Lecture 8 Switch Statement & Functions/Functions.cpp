a31b297dc986f81c8dcfd7a8e5f4b97e8dded6e3
hassamaziz <hassamaziz001@gmail.com>
2023-12-26 23:15:23 +0500

    Functions

---
+++ Lecture 8 Switch Statement & Functions/Functions.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+int power(int a,int b){
+    int ans = 1;
+    for (int i = 1; i <=b; i++)
+    {
+           ans = ans * a;
+    }
+    return ans;
+    
+}
+
+int power2(){
+
+    cout<<"Enter base number: ";
+   int num;
+    cin>>num;
+    cout<<"\nEnter exponent: ";
+   int exp;
+    cin>>exp;
+    int ans = 1;
+    for (int i = 1; i <=exp; i++)
+    {
+           ans = ans * num;
+    }
+    return ans;
+    
+}
+
+int isEven(int a){
+  
+  if(a&1){
+    return 0;
+  }
+    return 1;
+  
+}
+
+int main(){
+    // cout<<"Enter the base number: ";
+    // int num;
+    // cin>>num;
+    // cout<<"\nEnter the exponent: ";
+    // int exp;
+    // cin>>exp;
+    // int answer =  power(num,exp);
+
+    // cout << answer;
+
+
+    // int answer2 = power2();
+    // cout << "\n\nThe result is : "<<answer2 ;
+
+    int num;
+    cin >> num;
+    if(isEven(num)){
+        cout << "Your number is even";
+    }
+    else{
+        cout << "Your number is odd";
+    }
+
+}
