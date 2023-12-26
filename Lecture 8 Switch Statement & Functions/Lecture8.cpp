e3c5bdd49d9ca52dea7c929cf8f0993135b276e8
hassamaziz <hassamaziz001@gmail.com>
2023-12-26 19:02:31 +0500

    Switch statement understanding

---
+++ Lecture 8 Switch Statement & Functions/Lecture8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+                                            // Switch Statement
+    // int i = 1;
+    // switch (i)
+    // {
+    // case 1: cout << "ONE"; 
+
+    //     break;
+    // case 2: cout << "TWO"; 
+
+    //     break;
+    
+    // default: cout << "Default";
+    //     break;
+    // }         
+    
+                                   
+}
