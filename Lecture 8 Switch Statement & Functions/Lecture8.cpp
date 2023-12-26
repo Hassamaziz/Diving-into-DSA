65f1a286c89dc787e01c9b8df815c5db22a68199
hassamaziz <hassamaziz001@gmail.com>
2023-12-26 19:21:57 +0500

    Simple Calculator using Switch

--- Lecture 8 Switch Statement & Functions/Lecture8.cpp
+++ Lecture 8 Switch Statement & Functions/Lecture8.cpp
@@ -15,7 +15,56 @@ int main(){
     
     // default: cout << "Default";
     //     break;
-    // }         
+    // }      
+
+                                        // Exit keyword
+//     while(1)
+// {
+//   cout<<"HOW YOU DOIN????"<<endl;
+
+//   int num=1;
+//   switch(num)
+//   {
+//     case 1: cout<<"I'M FINE"<<endl;
+//     break;
+//   }
+//   exit(0);
+// }
+
+//                                     // Simple Calculator
+//     int a,b;
+//     cout << "enter value of a: ";
+//     cin >> a; 
+//     cout << "enter value of b: ";
+//     cin >> b; 
+//    char oper;
+//     cout << "enter the operator: ";
+//     cin >> oper;
+
+//     switch (oper)
+//     {
+//     case '+':
+//         cout<< "Answer is " << a+b;
+//         break;
+//          case '-':
+//         cout << "Answer is " << a-b;
+//         break;
+//          case '/':
+//         cout << "Answer is " << a/b;
+//         break;
+//        case '*':
+//        cout << "Answer is " << a*b;
+//        break;
+//         case '%':
+//        cout << "Answer is " << a%b;
+//        break;
+
     
-                                   
+//     default:
+//     cout << "Invalid Operator";
+//     break;
+//     }
+                                     
+
+
 }
