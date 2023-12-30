b0cac01d0cf450e6ee752d9b2bfda0c8c5c9372f
hassamaziz <hassamaziz001@gmail.com>
2023-12-30 14:58:42 +0500

    Array Intersection

---
+++ Lecture 10 Solving LeetCodeCodeStudio Questions [Arrays]/findIntersection.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int main() {
+    int i = 0;
+    int j = 0;
+    int arr1[5] = {1, 2, 2, 3, 3};
+    int arr2[3] = {1, 2, 3};
+    vector<int> ans;
+
+    while (i < 5 && j < 3) {
+        if (arr1[i] == arr2[j]) {
+            ans.push_back(arr1[i]);
+            i++;
+            j++;
+        } else if (arr1[i] < arr2[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    // Print the elements in the ans vector
+    for (int k = 0; k < ans.size(); k++) {
+        cout << ans[k] << " ";
+    }
+
+    return 0;
+}
