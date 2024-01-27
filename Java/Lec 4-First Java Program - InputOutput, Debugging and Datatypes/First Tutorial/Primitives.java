469eab63e3847203585c167a1a598734dd57cbe3
hassamaziz <hassamaziz001@gmail.com>
2024-01-27 18:39:54 +0500

    Primitives

---
+++ Java/Lec 4-First Java Program - InputOutput, Debugging and Datatypes/First Tutorial/Primitives.java
@@ -0,0 +1,23 @@
+public class Primitives {
+
+    public static void main(String[] args) {
+        // int myInt = 10;
+        // double myDouble = 9089987987.7834;
+        // // String is not primitive
+        // // String myStr = "hello world";
+        // char myChar = 'H';
+        // boolean myBoolean = false;
+        // float myFloat = 69.99f;
+        // long myLongInt = 1234352345342353453L;
+        
+        // System.out.println("Integer: " + myInt);
+        // System.out.println("Double: " + myDouble);
+        // System.out.println("Character: " + myChar);
+        // System.out.println("Boolean: " + myBoolean);
+        // System.out.println("Float: " + myFloat);
+        // System.out.println("Long Integer: " + myLongInt);
+
+
+    }
+    
+}
