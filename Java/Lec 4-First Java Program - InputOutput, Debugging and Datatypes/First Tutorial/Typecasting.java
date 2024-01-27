501423c5331185560d893674f7512624f7775714
hassamaziz <hassamaziz001@gmail.com>
2024-01-27 19:15:26 +0500

    Typecasting

---
+++ Java/Lec 4-First Java Program - InputOutput, Debugging and Datatypes/First Tutorial/Typecasting.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // int myInt = input.nextInt();
+        // float myFloat = input.nextFloat();
+
+
+        // Type Casting 
+        // int myInt = (int)(66.99);
+        // System.out.println("Integer Value: " + myInt);
+
+
+// int a = 258;
+// byte b = (byte)(a);
+// System.out.println(b);
+
+byte a = 40;
+byte b = 50;
+byte c = 100;
+int d = a*b/c;
+
+System.out.println(d);
+
+
+
+    }
+}
