522d0ef314d8dd8f656b9111a587909e4fe4c904
hassamaziz <hassamaziz001@gmail.com>
2024-01-28 12:09:41 +0500

    2 Assignment Questions Done

---
+++ Java/Lec 4-First Java Program - InputOutput, Debugging and Datatypes/Assignments/Assignments.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Assignments {
+    public static void main(String[] args) {
+            // Question 1: Write a program to print whether a number is even or odd, also take input from the user.
+  
+    // Scanner input = new Scanner(System.in);
+    // System.out.print("Enter an integer: ");
+    // int num = input.nextInt();
+    // if (num % 2 == 0) {
+    //     System.out.println("Number is Even");
+    // }else{
+    //     System.out.println("Number is odd");
+    // }
+
+
+    // Question 2: Take name as input and print a greeting message for that particular name.
+
+    // Scanner scn = new Scanner(System.in);
+    // System.out.print("Please enter your name : ");
+    // String name = scn.nextLine();
+    // System.out.printf("Hello "+ name + " Welcome!");
+
+
+
+    
+
+
+    }
+}
