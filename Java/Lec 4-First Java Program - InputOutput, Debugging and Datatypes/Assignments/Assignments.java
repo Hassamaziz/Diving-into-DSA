cfbba74f69daf452fd8a584bf82ee26a6043e25e
hassamaziz <hassamaziz001@gmail.com>
2024-01-28 12:29:16 +0500

    4 assignment questions done

--- Java/Lec 4-First Java Program - InputOutput, Debugging and Datatypes/Assignments/Assignments.java
+++ Java/Lec 4-First Java Program - InputOutput, Debugging and Datatypes/Assignments/Assignments.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Assignments {
     public static void main(String[] args) {
-            // Question 1: Write a program to print whether a number is even or odd, also take input from the user.
+
+    //         // Question 1: Write a program to print whether a number is even or odd, also take input from the user.
   
     // Scanner input = new Scanner(System.in);
     // System.out.print("Enter an integer: ");
@@ -14,7 +15,7 @@ public class Assignments {
     // }
 
 
-    // Question 2: Take name as input and print a greeting message for that particular name.
+    // // Question 2: Take name as input and print a greeting message for that particular name.
 
     // Scanner scn = new Scanner(System.in);
     // System.out.print("Please enter your name : ");
@@ -23,7 +24,53 @@ public class Assignments {
 
 
 
+//     // Question 3: Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+// Scanner input = new Scanner(System.in);
+// System.out.println("Enter Principal:");
+// double p = input.nextDouble();
+// System.out.println("Enter Time in years:");
+// double t = input.nextDouble();
+// System.out.println("Enter Rate of interest:");
+// double r = input.nextDouble();
+// double SimpleInterest = (p * t * r) / 100;
+
+// System.out.println("The simple interest rate is " + SimpleInterest);
+
+
+
+    //    // Question 4: Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+
+
+    //    Scanner input = new Scanner(System.in);
+    //    System.out.println("Enter First Number: ");
+    //    int x = input.nextInt();
+    //    System.out.println("Enter the Operator: ");
+    //    char operator = input.next().charAt(0);
+    //    System.out.println("Enter the second number: ");
+    //    int y = input.nextInt();
+    //    if (operator=='-') {
+    //     System.out.println("Result= " + (x - y));
+    //    }
+    //     else if (operator == '+') {
+    //        System.out.println("Result= " + (x + y));
+    //    } 
+    //    else if (operator == '/') {
+    //        if (y != 0) {
+    //            System.out.println("Result=" + (x / y));
+    //        } else {
+    //            System.out.println("Error! Division by zero not allowed.");
+    //        }
+    //    }
+    //     else if (operator == '*') {
+    //        System.out.println("Result=" + (x * y));
+    //    }
+    //     else {
+    //        System.out.println("Invalid operator");
+    //    }
+        
     
+       
 
 
     }
