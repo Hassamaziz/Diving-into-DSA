fa7d3366d827e7d123b047b6dd7813a4be0203e8
hassamaziz <hassamaziz001@gmail.com>
2024-01-28 11:43:15 +0500

    Typecasting

--- Java/Lec 4-First Java Program - InputOutput, Debugging and Datatypes/First Tutorial/Typecasting.java
+++ Java/Lec 4-First Java Program - InputOutput, Debugging and Datatypes/First Tutorial/Typecasting.java
@@ -16,14 +16,34 @@ public class Typecasting {
 // byte b = (byte)(a);
 // System.out.println(b);
 
-byte a = 40;
-byte b = 50;
-byte c = 100;
-int d = a*b/c;
+// byte a = 40;
+// byte b = 50;
+// byte c = 100;
+// int d = a*b/c;
 
-System.out.println(d);
+// System.out.println(d);
+
+// Automatic typeconversion
 
 
+// int num = 'B';
+// System.out.println(num);
+
+
+
+// System.out.println("안녕하세요");
+
+
+byte b = 42;
+char c = 'a';
+short s = 1024;
+int i = 50000;
+float f = 5.67f;
+double d = 0.1234;
+double result = (f * b) + (i / c) - (d * s);
+// float + int - double = double
+System.out.println((f * b) + " " + (i / c) + " " + (d * s));
+System.out.println(result);
 
     }
 }
