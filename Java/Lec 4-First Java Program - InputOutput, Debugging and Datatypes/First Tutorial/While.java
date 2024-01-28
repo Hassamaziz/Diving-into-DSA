68b257a0695c04bc227af895b058ac72bf0762a3
hassamaziz <hassamaziz001@gmail.com>
2024-01-28 11:47:10 +0500

    Basic If Else condition And While loop

---
+++ Java/Lec 4-First Java Program - InputOutput, Debugging and Datatypes/First Tutorial/While.java
@@ -0,0 +1,18 @@
+public class While {
+    public static void main(String[] args) {
+
+        // If condition
+        // int num = 22;
+
+
+        // if (num == 22){
+        //     System.out.println("Yes This is True");
+        // }
+        // else{
+        //     System.out.println("no this is False");
+        // }
+
+
+
+    }
+}
