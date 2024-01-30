11ad190676d6d9087f0c1dd95b258425ae36b5ea
hassamaziz <hassamaziz001@gmail.com>
2024-01-30 18:59:00 +0500

    For loops

--- Java/Lec 5-Conditionals and Loops + Calculator Program/Loops.java
+++ Java/Lec 5-Conditionals and Loops + Calculator Program/Loops.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
         //    For Loop
 
-        for (int i = 1; i <=5; i++) {
-            System.out.println("Hello World");
-        }
+        // for (int i = 1; i <=5; i++) {
+        //     System.out.println(i);
+        // }
+
+        // Scanner input = new Scanner(System.in);
+        // int num = input.nextInt();
+        // for (int i = 1; i <= num; i++) {
+
+        //     System.out.print(i + " ");
+            
+        // }
+
+        
     }
 }
