c9d07d9993cc665080a36e36bd7d855b4e192a29
hassamaziz <hassamaziz001@gmail.com>
2024-01-30 18:54:18 +0500

    Loops

---
+++ Java/Lec 5-Conditionals and Loops + Calculator Program/Loops.java
@@ -0,0 +1,9 @@
+public class Loops {
+    public static void main(String[] args) {
+        //    For Loop
+
+        for (int i = 1; i <=5; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
