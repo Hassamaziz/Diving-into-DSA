252930dc79aaa0c8b54e0c016493cc7f29d5c621
hassamaziz <hassamaziz001@gmail.com>
2024-01-30 18:37:16 +0500

    If else If

--- Java/Lec 5-Conditionals and Loops + Calculator Program/Main.java
+++ Java/Lec 5-Conditionals and Loops + Calculator Program/Main.java
@@ -2,17 +2,20 @@ public class Main {
     public static void main(String[] args) {
 // //                     If statement
 
-//         int sal = 15000;
+        int sal = 25000;
 
-//         if (sal >10000) {
+        if (sal >10000 && sal < 20000) {
 
-//             sal +=2000;
+            sal +=2000;
             
-//         }
-//         else{
-//             sal +=1000;
-//         }
-//         System.out.println("Salary actual: " + sal);
+        }
+        else if (sal >20000){
+            sal +=5000;
+        }
+        else{
+            sal +=1000;
+        }
+        System.out.println("Salary actual: " + sal);
 
-//     }
+    }
 }
