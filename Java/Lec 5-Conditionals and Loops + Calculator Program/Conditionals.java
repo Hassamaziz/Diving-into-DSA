afede663c9a1a03c6c62f49bf8feb2607c282a6b
hassamaziz <hassamaziz001@gmail.com>
2024-01-30 18:51:53 +0500

    Conditionals

---
+++ Java/Lec 5-Conditionals and Loops + Calculator Program/Conditionals.java
@@ -0,0 +1,21 @@
+public class Conditionals {
+    public static void main(String[] args) {
+// //                     If else If statement
+
+    //     int sal = 25000;
+
+    //     if (sal >10000 && sal < 20000) {
+
+    //         sal +=2000;
+            
+    //     }
+    //     else if (sal >20000){
+    //         sal +=5000;
+    //     }
+    //     else{
+    //         sal +=1000;
+    //     }
+    //     System.out.println("Salary actual: " + sal);
+
+    }
+}
