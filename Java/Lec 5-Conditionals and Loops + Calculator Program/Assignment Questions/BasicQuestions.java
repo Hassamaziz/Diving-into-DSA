68ee792240f83e9495096611ca45074910aa1edb
hassamaziz <hassamaziz001@gmail.com>
2024-02-02 19:05:41 +0500

    Question 2 done

--- Java/Lec 5-Conditionals and Loops + Calculator Program/Assignment Questions/BasicQuestions.java
+++ Java/Lec 5-Conditionals and Loops + Calculator Program/Assignment Questions/BasicQuestions.java
@@ -7,7 +7,7 @@ public class BasicQuestions{
         // // Question 1: Area Of Circle Java Program
 
         // Scanner input = new Scanner(System.in);
-        // System.out.println("Enter the radius of a circle: ");
+        // System.out.print("Enter the radius of a circle: ");
         // double r = input.nextDouble();
         // double area = Math.PI * (r*r);
         // System.out.println("The area of the circle is : "+ area);
@@ -15,13 +15,25 @@ public class BasicQuestions{
         // //  Question 2: Area Of Triangle
 
         // Scanner input  = new Scanner(System.in);
-        // System.out.println("Enter the Base of Triangle");
+        // System.out.print("Enter the Base of Triangle");
         // Double base = input.nextDouble();
-        // System.out.println("Enter the Height of Triangle");
+        // System.out.print("Enter the Height of Triangle");
         // Double height = input.nextDouble();
 
         // Double area = (base*height)/2;
         // System.out.println("The area of triangle is " +area);
+
+        // // Question 3: Area Of Rectangle Program
+        
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.print("Enter length of rectangle: ");
+        // double len = input.nextDouble();
+        // System.out.print("Enter width of rectangle: ");
+        // double wid = input.nextDouble();
+        // double area = len * wid;
+
+        // System.out.println("The area of the rectangle is: " + area);
 
     }
 }
