1caea256646962e7b08c0c0b49cb047a002f1f0c
hassamaziz <hassamaziz001@gmail.com>
2024-02-02 19:22:17 +0500

    Question 7 done

--- Java/Lec 5-Conditionals and Loops + Calculator Program/Assignment Questions/BasicQuestions.java
+++ Java/Lec 5-Conditionals and Loops + Calculator Program/Assignment Questions/BasicQuestions.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 
 public class BasicQuestions{
     public static void main(String[] args) {
+
         // // Question 1: Area Of Circle Java Program
+
 
         // Scanner input = new Scanner(System.in);
         // System.out.print("Enter the radius of a circle: ");
         // double r = input.nextDouble();
-        // double area = Math.PI * (r*r);
+
+        // if (r <= 0) {
+        //     System.out.println("Invalid Input, Please enter positive numbers only.");
+        // } else {
+        //   double area = Math.PI * (r*r);
         // System.out.println("The area of the circle is : "+ area);
+        // }
+        
+
+
+
 
         // //  Question 2: Area Of Triangle
 
@@ -20,8 +31,19 @@ public class BasicQuestions{
         // System.out.print("Enter the Height of Triangle");
         // Double height = input.nextDouble();
 
-        // Double area = (base*height)/2;
+        // if (base <= 0 || height <= 0) {
+        //     System.out.println("Invalid Input, Please enter positive numbers only.");
+        // } else {
+        //  Double area = (base*height)/2;
         // System.out.println("The area of triangle is " +area);
+        // }
+
+       
+
+
+
+
+
 
         // // Question 3: Area Of Rectangle Program
         
@@ -31,11 +53,21 @@ public class BasicQuestions{
         // double len = input.nextDouble();
         // System.out.print("Enter width of rectangle: ");
         // double wid = input.nextDouble();
-        // double area = len * wid;
 
-        // System.out.println("The area of the rectangle is: " + area);
+        // if (len <= 0 || wid <= 0) {
+        //     System.out.println("Invalid Input, Please enter positive numbers only.");
+        // } else {
+        //     double area = len * wid;
+        //   System.out.println("The area of the rectangle is: " + area);
+        // }
+       
 
-        // Question 4: Area Of Isosceles Triangle 
+       
+
+
+
+
+        // // Question 4: Area Of Isosceles Triangle 
 
         // Scanner input  = new Scanner(System.in);
         // System.out.print("Enter the Base of Isosceles Triangle");
@@ -43,20 +75,65 @@ public class BasicQuestions{
         // System.out.print("Enter the Height of Isosceles Triangle");
         // Double height = input.nextDouble();
 
-        // Double area = (base*height)/2;
-        // System.out.println("The area of isosceles triangle is " +area);
+        // if (base <= 0 || height <= 0) {
+        //     System.out.println("Invalid Input, Please enter positive numbers only.");
+        // } else {
+        //     Double area = (base*height)/2;
+        //     System.out.println("The area of isosceles triangle is " +area);
+        // }
 
-        // Question 5 : Area Of Parallelogram
+        
+       
 
-         // Scanner input = new Scanner(System.in);
+
+
+        //  // Question 5 : Area Of Parallelogram
+
+        //  Scanner input = new Scanner(System.in);
 
         // System.out.print("Enter length of Parallelogram : ");
         // double len = input.nextDouble();
         // System.out.print("Enter width of Parallelogram : ");
         // double wid = input.nextDouble();
-        // double area = len * wid;
+        // if (len <= 0 || wid <= 0) {
+        //     System.out.println("Invalid Input, Please enter positive numbers only.");
+        // } else {
+        //     double area = len * wid;
+        //     System.out.println("The area of the Parallelogram is: " + area);
+        // }
 
-        // System.out.println("The area of the Parallelogram is: " + area);
+       
+
+        
+
+
+        // // Question 6 : Area Of Rhombus
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter the length of a rhombus's diagonals: ");
+        // double dg = input.nextDouble();
+        // System.out.print("Enter the side of a rhombus: ");
+        // double s = input.nextDouble();
+        // if (dg <= 0 || s <= 0) {
+        //     System.out.println("Invalid Input, Please enter positive numbers only.");
+        // } else {
+        //     double area = 0.5 * dg * s;
+        //     System.out.println("The area of the rhombus is: " + area);
+        // }
+
+
+
+        // // Question 7 : Area of Equilateral Triangle
+        
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter the side length of an equilateral triangle: ");
+        // double l = input.nextDouble();
+        // if (l <= 0) {
+        //     System.out.println("Side length must be greater than zero");
+        // } else {
+        //     double area = Math.sqrt(3) / 4 * (l*l);
+        //     System.out.printf("The area of the equilateral triangle is " +area);
+        // }
+
 
 
     }
