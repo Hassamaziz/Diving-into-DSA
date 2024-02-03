08771806f95903bc8eef177359f1eb5ade772f8b
hassamaziz <hassamaziz001@gmail.com>
2024-02-03 16:11:54 +0500

    Question 23 done

--- Java/Lec 5-Conditionals and Loops + Calculator Program/Assignment Questions/BasicQuestions.java
+++ Java/Lec 5-Conditionals and Loops + Calculator Program/Assignment Questions/BasicQuestions.java
@@ -376,5 +376,21 @@ public class BasicQuestions{
 
 
 
+
+
+
+        // //  Question 23 : Input a number and print all the factors of that number (use loops).
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Please enter a positive integer: ");
+        // int num = scanner.nextInt();
+        // System.out.println("The factors of " +num+ " are:");
+        // for (int i = 1; i <=num ; i++) {
+        //     if (num % i == 0) {
+        //         System.out.print(i+"  ");
+        //     }
+        // }
+
+
     }
 }
