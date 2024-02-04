e21a642c216b6230ad9e6770eb43fdf08eee8d2e
hassamaziz <hassamaziz001@gmail.com>
2024-02-04 15:05:36 +0500

    First Intermediate Question done

---
+++ Java/Lec 5-Conditionals and Loops + Calculator Program/Assignment Questions/IntermediatePrograms.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IntermediatePrograms {
+    public static void main(String[] args) {
+        // // Question 1:Factorial Program In Java
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter a number to calculate its factorial : ");
+        // int num = input.nextInt();
+        // long fact = 1;
+        // for (int i = 1; i <= num; ++i) {
+        //     fact *= i;
+        // }
+        // System.out.println("The Factorial of " + num + " is : " + fact);
+
+    }
+}
