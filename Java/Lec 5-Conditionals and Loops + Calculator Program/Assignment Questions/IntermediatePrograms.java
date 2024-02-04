f6f3e8d5830e3ed0058ddb121ccadc9c741c1ec4
hassamaziz <hassamaziz001@gmail.com>
2024-02-04 15:09:06 +0500

    Question 2 done

--- Java/Lec 5-Conditionals and Loops + Calculator Program/Assignment Questions/IntermediatePrograms.java
+++ Java/Lec 5-Conditionals and Loops + Calculator Program/Assignment Questions/IntermediatePrograms.java
@@ -12,5 +12,19 @@ public class IntermediatePrograms {
         // }
         // System.out.println("The Factorial of " + num + " is : " + fact);
 
+
+        // //  Question 2:Calculate Average Of N Numbers
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("How many numbers do you want to enter?");
+        // int n = sc.nextInt();
+        // double sum = 0;
+        // System.out.println("Enter the numbers one by one:");
+        // for (int i = 1; i <=n ; i++) {
+        //     double num = sc.nextDouble();
+        //     sum += num;
+        // }
+        // double avg = sum / n;
+        // System.out.printf("The average of %d numbers is %.2f", n, avg);
+
     }
 }
