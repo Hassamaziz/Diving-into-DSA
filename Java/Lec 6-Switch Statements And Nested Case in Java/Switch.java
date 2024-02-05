8589e8fbe61186d9087f025a7a511616022863ba
hassamaziz <hassamaziz001@gmail.com>
2024-02-05 13:14:37 +0500

    Enchanced Switch Statements understanding

--- Java/Lec 6-Switch Statements And Nested Case in Java/Switch.java
+++ Java/Lec 6-Switch Statements And Nested Case in Java/Switch.java
@@ -4,6 +4,8 @@ public class Switch{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         // System.out.print("Enter a fruit name: ");
+
+                                          // NORMAL SWITCH STATEMENT 
         // String fruit = input.nextLine(); 
         // switch (fruit) {
         //     case "mango":
@@ -25,6 +27,8 @@ public class Switch{
 
         System.out.println("Enter a Day");
         int day = input.nextInt();
+
+                              // ENHANCED SWITCH STATEMENT
         // switch (day) {
         //     case 1 ->System.out.println("Monday");
         //     case 2 ->System.out.println("Tuesday");
@@ -36,23 +40,32 @@ public class Switch{
         //     default -> System.out.println("enter a valid day number");
     // }
 
-        switch (day) {
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            System.out.println("Its a Weekday");
+        // switch (day) {
+        //     case 1:
+        //     case 2:
+        //     case 3:
+        //     case 4:
+        //     case 5:
+        //     System.out.println("Its a Weekday");
                 
-                break;
-            case 6:
-            case 7:
-            System.out.println("Its a Weekend");  
-            break;  
+        //         break;
+        //     case 6:
+        //     case 7:
+        //     System.out.println("Its a Weekend");  
+        //     break;  
         
-            default:
-            System.out.println("Enter valid day");
+        //     default:
+        //     System.out.println("Enter valid day");
                 
+        // }
+
+
+        switch (day) {
+            case 1,2,3,4,5 -> System.out.println("Its a weekday");
+            case 6,7 -> System.out.println("Its a Weekend");
+            default -> System.out.println("enter valid day"); 
+        
+          
         }
                 
               
