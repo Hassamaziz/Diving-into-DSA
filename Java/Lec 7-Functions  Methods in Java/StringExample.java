2bc48b1d43ee318942741b4835295818da9c1358
hassamaziz <hassamaziz001@gmail.com>
2024-02-06 18:04:20 +0500

    String

--- Java/Lec 7-Functions  Methods in Java/StringExample.java
+++ Java/Lec 7-Functions  Methods in Java/StringExample.java
@@ -2,9 +2,28 @@ import java.util.Scanner;
 
 public class StringExample {
     public static void main(String[] args) {
-      System.out.println(example());
+    //   System.out.println(example());
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String userName = input.nextLine();
+    System.out.println(myGreet(userName));
+
+   
+    // System.out.println(myGreet("Hassam"));
+
+    
 
     }
+
+    
+
+   static String myGreet(String name) {
+       String message = "Hello " + name;
+       return message;
+    }
+
+
+
     static String example(){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a string: ");
