b1010fd0d7c8c2e0682d000c294763ef97ae6248
hassamaziz <hassamaziz001@gmail.com>
2024-02-07 23:20:24 +0500

    Change value

--- Java/Lec 7-Functions  Methods in Java/ChangeValue.java
+++ Java/Lec 7-Functions  Methods in Java/ChangeValue.java
@@ -2,13 +2,15 @@ import java.util.Arrays;
 
 public class ChangeValue {
     public static void main(String[] args) {
-        int[] arr = {2,4,6,8,10};
-        ChangeValue(arr);
-        System.out.println(Arrays.toString(arr));
+    //     int[] arr = {2,4,6,8,10};
+    //     ChangeValue(arr);
+    //     System.out.println(Arrays.toString(arr));
 
-    }
+    // }
 
-    static void ChangeValue(int[] nums){
-        nums[0]=24;
+    // static void ChangeValue(int[] nums){
+    //     nums[0]=2;
+
+
     }
 }
