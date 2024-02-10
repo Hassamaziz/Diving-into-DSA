acdb442371fb86b08f6ffefaf0941901e3a03ad1
hassamaziz <hassamaziz001@gmail.com>
2024-02-10 19:45:57 +0500

    Overloading

--- Java/Lec 7-Functions  Methods in Java/Overloading.java
+++ Java/Lec 7-Functions  Methods in Java/Overloading.java
@@ -2,6 +2,8 @@ public class Overloading {
     public static void main(String[] args) {
         fun(44);
         fun("hassam");
+       System.out.println( sum(2, 5));
+       System.out.println(sum(2,4,5));
     }
     static void fun(int n){
 System.out.println(n);
@@ -9,4 +11,11 @@ System.out.println(n);
     static void fun(String s){
 System.out.println(s);
     }
+
+    static int sum(int a,int b){
+return a+b;
+    }
+    static int sum(int a,int b,int c){
+        return a+b+c;
+            }
 }
