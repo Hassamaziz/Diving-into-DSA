f196694e91c9de7436d7123f3d5fde08c7b638c5
hassamaziz <hassamaziz001@gmail.com>
2024-02-11 17:13:17 +0500

    array inputs

---
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/code/Input.java
@@ -0,0 +1,20 @@
+package code;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter 5 integers : ");
+        for (int i=0 ;i<arr.length ;i++) {
+        arr[i] = input.nextInt();
+    }
+
+    System.out.println( "The entered numbers are:");
+    
+    //displaying the array elements
+    System.out.println(Arrays.toString(arr));
+}
+}
