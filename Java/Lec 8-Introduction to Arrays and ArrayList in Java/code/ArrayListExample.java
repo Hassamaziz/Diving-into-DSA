876a82276d5b7c445f36ad2216a9c5dbe9e8019d
hassamaziz <hassamaziz001@gmail.com>
2024-02-12 18:30:05 +0500

    ArrayList

---
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/code/ArrayListExample.java
@@ -0,0 +1,25 @@
+package code;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(5);
+        list.add(34);
+        list.add(674);
+        list.add(34644);
+        list.add(243434);
+        list.add(87634);
+        list.add(34);
+        list.add(674);
+        list.add(34644);
+        list.add(243434);
+        list.add(87634);
+        list.add(34);
+      
+
+        System.out.println(list); 
+        
+
+    }
+}
