d33f63ae067d11607ed2c80705a2bce39004f6ff
hassamaziz <hassamaziz001@gmail.com>
2024-02-12 18:02:59 +0500

    inputs and outputs of multidimensional arrays

--- Java/Lec 8-Introduction to Arrays and ArrayList in Java/code/MultiDimensionalArray.java
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/code/MultiDimensionalArray.java
@@ -1,8 +1,39 @@
 package code;
 
+import java.util.Scanner;
+
 public class MultiDimensionalArray {
     public static void main(String[] args) {
-        int [][] MultiArr = new int [4][4];
-        
+        Scanner input = new Scanner(System.in);
+        // int[][] MultiArr =  
+        //          { {0,1,2,3},
+        //            {4,5,6,7},
+        //            {8,9,10,11}};
+
+
+        // Inputs 
+        int [][] arr = new int[3][3];
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = input.nextInt();
+                
+            }
+           
+            
+        }
+
+
+                       // Displaying array 
+
+        // for (int row = 0; row < arr.length; row++) {
+        //     for (int col = 0; col < arr[row].length; col++) {
+        //       System.out.print(arr[row][col] + " ");
+                
+        //     }
+            
+        // }
+
+
     }
 }
