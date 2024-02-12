15b476223fe7fd801e78a8111ab81425cb5f0c22
hassamaziz <hassamaziz001@gmail.com>
2024-02-12 19:19:12 +0500

    Max value in given range

--- Java/Lec 8-Introduction to Arrays and ArrayList in Java/code/MaxItem.java
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/code/MaxItem.java
@@ -1,25 +1,52 @@
 package code;
 
+import java.util.Scanner;
+
 public class MaxItem {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int[] arr = {23,5,74,2,5,6};
-        System.out.println(maxItem(arr));
+
+
+                    //  To find max in a given range
+
+        System.out.println("Enter the range from where you want to see the maximum value : ");
+        int start = input.nextInt();
+        int end = input.nextInt();
+        if (start < 0 || end >= arr.length || start > end) {
+            System.out.println("Invalid Input");
+        } else {
+            System.out.println(maxInRange(arr, start, end));
+        }
+
+
+
+        // System.out.println(maxItem(arr));
     }
-    static int maxItem(int[] arr){  
-        if (arr == null || arr.length <= 1) return -1;
-        int maxVal = arr[0]; 
-        for (int i = 0; i < arr.length; i++) {
+
+
+
+     static int maxInRange(int[] arr,int a,int b){  
+        
+        int maxVal = arr[a]; 
+        for (int i = a; i <= b; i++) {
             if (maxVal < arr[i])
              {
                  maxVal = arr[i];
             }
-            
-
         }
-
-
         return maxVal;
-
-
     }
+
+    // static int maxItem(int[] arr){  
+    //     if (arr == null || arr.length <= 1) return -1;
+    //     int maxVal = arr[0]; 
+    //     for (int i = 0; i < arr.length; i++) {
+    //         if (maxVal < arr[i])
+    //          {
+    //              maxVal = arr[i];
+    //         }
+    //     }
+    //     return maxVal;
+    // }
 }
