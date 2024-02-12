c09598a557245adee65f535d2712f1bb786d413d
hassamaziz <hassamaziz001@gmail.com>
2024-02-12 18:38:43 +0500

    Inputs and Outputs in ArrayList

--- Java/Lec 8-Introduction to Arrays and ArrayList in Java/code/ArrayListExample.java
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/code/ArrayListExample.java
@@ -1,36 +1,53 @@
 package code;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExample {
+
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(5);
-        list.add(34);
-        list.add(674);
-        list.add(34644);
-        list.add(243434);
-        list.add(87634);
-        list.add(34);
-        list.add(674);
-        list.add(34644);
-        list.add(243434);
-        list.add(87634);
-        list.add(34);
+
+        Scanner input = new Scanner(System.in);
+        // ArrayList<Integer> list = new ArrayList<>(5);
+        // list.add(34);
+        // list.add(674);
+        // list.add(34644);
+        // list.add(243434);
+        // list.add(87634);
+        // list.add(34);
+        // list.add(674);
+        // list.add(34644);
+        // list.add(243434);
+        // list.add(87634);
+        // list.add(34);
       
 
-        System.out.println(list); 
+        // System.out.println(list); 
 
-        System.out.println(list.contains(34));
+        // System.out.println(list.contains(34));
 
-        list.set(0, 69);
+        // list.set(0, 69);
 
-        System.out.println(list); 
+        // System.out.println(list); 
 
-        list.remove(4);
+        // list.remove(4);
 
-        System.out.println(list);
-        
+        // System.out.println(list);
 
+
+
+        // Input and Output of ArrayList
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+    for (int i = 0; i < 7; i++) {
+        arr.add(input.nextInt());
+    }
+
+
+    // Output 
+    for (int i = 0; i < 7; i++) {
+        System.out.print(arr.get(i) + "   ");
+    }
         
 
     }
