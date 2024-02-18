caf4bc16f722386334bafc7e6f4860509e176aaf
hassamaziz <hassamaziz001@gmail.com>
2024-02-18 19:05:40 +0500

    Question 15 done

---
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/easy questions/Q-15 Diagonal Sum/DiagonalSumMatrix.java
@@ -0,0 +1,41 @@
+public class DiagonalSumMatrix {
+    public static void main(String[] args) {
+        /*
+         Given a square matrix mat, return the sum of the matrix diagonals.
+         Only include the sum of all the elements on the primary diagonal and all the
+        elements on the secondary diagonal that are not part of the primary diagonal.
+        Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+        Notice that element mat[1][1] = 5 is counted only once.
+         */
+
+
+        int[][] mat =
+        {{1,2,3},
+         {4,5,6},
+         {7,8,9}};
+        // { {1,1,1,1},
+        //   {1,1,1,1},
+        //   {1,1,1,1},
+        //   {1,1,1,1}};
+       
+          System.out.println(mat.length);
+          System.out.println("The sum of the primary diagonal is: " + findDiagonalSum(mat));
+    }
+    static int findDiagonalSum(int[][] mat){
+        int sum = 0;
+        
+        
+        for (int i=0 ; i < mat.length ;i++){
+             sum += mat[i][i];
+           if (i!=mat.length-i-1) {
+            sum+= mat[i][mat.length -i-1];
+            
+           }
+           
+        }    
+
+        return sum;
+
+    }
+    
+}
