9b72938c9b27ce5c9264b2701153d0a55a2ce5a5
hassamaziz <hassamaziz001@gmail.com>
2024-02-19 19:29:51 +0500

    question 16 done

---
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/easy questions/Q-16 Find Numbers with Even Number of Digits/FindNumberswithEvenNumberofDigits.java
@@ -0,0 +1,27 @@
+public class FindNumberswithEvenNumberofDigits {
+    public static void main(String[] args) {
+        // Find Numbers with Even Number of Digits
+        // Given an array nums of integers, return how many of them contain an even number of digits.
+        int[] nums = new int[]{12, 35, 22244444, 6789};
+        System.out.println("Count of numbers with even number of digits: " + findNumbersWithEvenNumberOfDigits(nums));
+    }
+    static int findNumbersWithEvenNumberOfDigits(int[] nums){
+        int count=0;
+        for(int i=0; i<nums.length; i++){
+            int n = nums[i];
+            int digit = 0;
+            while (n > 0){
+                n /= 10;
+                digit++;
+            }
+            if(digit % 2 == 0)
+                count++;
+    
+        
+        }
+        
+            
+        return count;
+
+    } 
+}
