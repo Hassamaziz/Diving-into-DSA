78e2a34dedf90a87fea223c7b3be25ea22482b97
hassamaziz <hassamaziz001@gmail.com>
2024-02-24 13:25:40 +0500

    Question 20 done

---
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/easy questions/Q-20 Matrix Rotation/MatrixRotation.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class MatrixRotation {
+public static void main(String[] args) {
+    // Determine Whether Matrix Can Be Obtained By Rotation
+    // Given two n x n binary matrices mat and target, return true if it is possible to make mat equal
+    //  to target by rotating mat in 90-degree increments, or false otherwise.
+    int[][] mat = {{0,0,0}, {0,1,0},{1,1,1}};
+    int[][] target = {{1,1,1}, {0,1,0},{0,0,0}};
+    System.out.println(canBeObtained(mat, target));
+}
+static boolean canBeObtained(int[][] mat, int[][] target) {
+    int c90=0,c180=0,c270=0,c360 = 0;
+    int n  = mat.length;
+    for (int i = 0; i <n;i++) {
+        for (int j = 0; j < n; j++) {
+            if (target[i][j]==mat[n-j-1][i]) 
+                c90++;  
+                if (target[i][j]==mat[n-i-1][n-j-1]) 
+                c180++; 
+                if (target[i][j]==mat[j][n-i-1]) 
+                c270++;
+                if (target[i][j]==mat[i][j]) 
+                c360++;            
+        }
+        
+    }
+    if (c90==n*n||c180==n*n||c270==n*n||c360==n*n) {
+        return true; }
+        else return false;
+   
+    
+   
+    
+   
+}
+
+}
