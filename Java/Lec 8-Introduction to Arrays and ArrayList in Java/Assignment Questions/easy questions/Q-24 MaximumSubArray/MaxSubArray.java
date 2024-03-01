a9b2835782376260b4b714567f0cc2f4cfee844a
hassamaziz <hassamaziz001@gmail.com>
2024-03-01 22:31:01 +0500

    Question 24 done

---
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/easy questions/Q-24 MaximumSubArray/MaxSubArray.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class MaxSubArray {
+        public static void main(String[] args) {
+            // Given an integer array nums, find the subarray with the largest sum, and return its sum
+            int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+            System.out.println("Subarray with largest sum: ");
+             System.out.println(maxSubArray(nums));
+           
+
+        }    
+        public static int maxSubArray(int[] nums) {
+            int maxSum = Integer.MIN_VALUE;
+            int currentSum = 0;
+           
+            
+            for (int i = 0; i < nums.length; i++) {
+                currentSum += nums[i];
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+                
+                if (currentSum < 0) {
+                    currentSum = 0;
+                }
+            }
+            
+            return maxSum;
+        
+
+            }
+
+}
