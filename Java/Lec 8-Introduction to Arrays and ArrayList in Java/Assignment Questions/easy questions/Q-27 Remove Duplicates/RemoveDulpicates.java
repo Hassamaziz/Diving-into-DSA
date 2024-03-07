d898dd598a53d3364020eedc5af084c63abe3899
hassamaziz <hassamaziz001@gmail.com>
2024-03-07 23:20:31 +0500

    ..

--- Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/easy questions/Q-27 Remove Duplicates/RemoveDulpicates.java
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/easy questions/Q-27 Remove Duplicates/RemoveDulpicates.java
@@ -27,7 +27,6 @@ public class RemoveDulpicates {
         // for (int i = 0; i < k; i++) {
         //     assert nums[i] == expectedNums[i];
         // }
-        // If all assertions pass, then your solution will be accepted.
         int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
       
         int k = removeDuplicates(nums);
