312de0538c759e51157434e2b5a04dd86c621fdf
hassamaziz <hassamaziz001@gmail.com>
2024-03-13 19:23:31 +0500

    Question 28 done

--- Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/easy questions/Q-28 Cost of Chips/CostOfMovingChips.java
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/easy questions/Q-28 Cost of Chips/CostOfMovingChips.java
@@ -1,3 +1,30 @@
 public class CostOfMovingChips {
+    public static void main(String[] args) {
+        // We have n chips, where the position of the ith chip is position[i].
+        // We need to move all the chips to the same position.
+        // In one step, we can change the position of the ith chip from position[i] to:
+        // position[i] + 2 or position[i] - 2 with cost = 0.
+        // position[i] + 1 or position[i] - 1 with cost = 1.
+        // Return the minimum cost needed to move all the chips to the same position.
+        int[] positions = new int[]{1,100000};
+        System.out.println("Minimum cost to move all chips to the same place is " + minCostToMoveAllChips(positions));
+        
+
+    }
+    static int minCostToMoveAllChips(int[] positions) {
+        if (positions == null || positions.length == 0) return 0;
+        int e=0;
+        int o=0;
+        for (int i : positions){
+            if (i%2==0) {
+                e++;
+            }
+            else{
+                o++;
+            }
+        }
+        return Math.min(e,o);
+
+    }
     
 }
