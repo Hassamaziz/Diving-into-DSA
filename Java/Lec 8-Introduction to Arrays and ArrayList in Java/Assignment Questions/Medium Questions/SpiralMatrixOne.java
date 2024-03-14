041352dd58e6b58f2b51691320318de86c10b76d
hassamaziz <hassamaziz001@gmail.com>
2024-03-14 20:41:09 +0500

    Medium question 1

---
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/Medium Questions/SpiralMatrixOne.java
@@ -0,0 +1,17 @@
+public class SpiralMatrixOne {
+    public static void main(String[] args) {
+        // Given an m x n matrix, return all elements of the matrix in spiral order.
+        int[][] mat = {{1,2,3}, {4,5,6}, {7,8,9}};
+        System.out.println("Spiral Order: ");
+        int[] res= printSpiralOrder(mat);
+
+        
+    }
+     static int[] printSpiralOrder(int[][] mainMatrix) {
+        int[] res = new int[mainMatrix.length];
+        
+        return res;
+       
+
+    }
+}
