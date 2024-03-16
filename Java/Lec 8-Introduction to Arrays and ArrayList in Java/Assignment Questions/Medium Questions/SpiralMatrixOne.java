afd4e4b690909a84bc10ce655354f2fca35310b3
hassamaziz <hassamaziz001@gmail.com>
2024-03-16 15:44:45 +0500

    Medium Question 1 done

--- Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/Medium Questions/SpiralMatrixOne.java
+++ Java/Lec 8-Introduction to Arrays and ArrayList in Java/Assignment Questions/Medium Questions/SpiralMatrixOne.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
+
 public class SpiralMatrixOne {
     public static void main(String[] args) {
         // Given an m x n matrix, return all elements of the matrix in spiral order.
-        int[][] mat = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
         System.out.println("Spiral Order: ");
-        int[] res= printSpiralOrder(mat);
-
-        
+        ArrayList<Integer> res= new ArrayList<>(printSpiralOrder(matrix)); 
+        for (int i : res){
+            System.out.print(i + " ");
+        }
     }
-     static int[] printSpiralOrder(int[][] mainMatrix) {
-        int[] res = new int[mainMatrix.length];
-        
+    
+    static ArrayList<Integer> printSpiralOrder(int[][] matrix) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int n = matrix.length, m = matrix[0].length;
+        int left = 0, up = 0, right = m - 1, down = n - 1;
+        while (res.size() < n * m) {
+            for (int i = left; i <= right && res.size() < n * m; i++) res.add(matrix[up][i]);
+            for (int i = up + 1; i <= down && res.size() < n * m; i++) res.add(matrix[i][right]);
+            for (int i = right - 1; i >= left && res.size() < n * m; i--) res.add(matrix[down][i]);
+            for (int i = down - 1; i > up && res.size() < n * m; i--) res.add(matrix[i][left]);
+            left++;
+            up++;
+            right--;
+            down--;
+        }
         return res;
-       
-
     }
 }
