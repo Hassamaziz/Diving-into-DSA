db2ccd70cbf36ea2bf1c88540587a268628cf334
hassamaziz <hassamaziz001@gmail.com>
2024-05-02 00:03:30 +0500

    Question 3 done

---
+++ NeetCode 150/Arrays & Hashings/Q-3 Two Sum/TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class TwoSum{
+    public static void main(String[] args) {
+        int arr[] = new int[]{44, 2, 11, 15};
+        int target = 17;
+        System.out.println("Indexes of the two elements whose sum is " + target);
+        printIndicesOfTwoElements(arr, target);
+    }
+    static void printIndicesOfTwoElements(int[] arr, int target){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0;i<arr.length;i++){
+            if (map.containsKey(target - arr[i])){
+                System.out.print("Pair ("+map.get(target-arr[i])+" , "+i+") ");
+            }else{
+                map.put(arr[i], i);
+            }
+        }
+    
+
+
+    }
+}
