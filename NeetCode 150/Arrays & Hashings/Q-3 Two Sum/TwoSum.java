596c17a2651447d49d792edd0e5ae63e78d6659f
Hassam <118983911+Hassamaziz@users.noreply.github.com>
2024-05-02 23:29:01 +0500

    Update TwoSum.java

--- NeetCode 150/Arrays & Hashings/Q-3 Two Sum/TwoSum.java
+++ NeetCode 150/Arrays & Hashings/Q-3 Two Sum/TwoSum.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 
 public class TwoSum{
     public static void main(String[] args) {
+        // For array two sum 
         int arr[] = new int[]{44, 2, 11, 15};
         int target = 17;
         System.out.println("Indexes of the two elements whose sum is " + target);
